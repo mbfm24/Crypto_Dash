0d0b5931d882e242604887b4d4c6a35d21535bc3
Dave <david.c.petersen@gmail.com>
2023-02-09 10:09:16 -0600

    Changed file names

---
+++ pages/1_CryptoData.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# Loading icon image using PIL
+img = Image.open('./Images/1f4c8.png')
+
+# Adding title and icon image to app
+st.set_page_config(
+    page_title='Overview',
+    page_icon=img
+)
+
+# Remove main menu and footer defaults
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+st.title("Crypto Dash")
+
+# Define a list of cryptocurrencies to display
+CRYPTOS = ["Bitcoin", "Ethereum", "Litecoin", "ZCash", "Chainlink","Doge"]
+
+# Load the data for each cryptocurrency from a CSV file
+def load_crypto_data(crypto):
+    file_name = f"./data/{crypto}.csv"
+    df = pd.read_csv(file_name)
+    df["crypto"] = crypto
+    return df
+
+# Concatenate all the data into a single dataframe
+df = pd.concat([load_crypto_data(crypto) for crypto in CRYPTOS])
+
+# Create a function for displaying the data for a selected cryptocurrency
+def display_crypto_data(selected_crypto):
+    st.write(df[df['crypto'] == selected_crypto])
+    st.line_chart(df[df['crypto'] == selected_crypto]['open'])
+    st.line_chart(df[df['crypto'] == selected_crypto]['close'])
+
+# Create a sidebar with links to different parts of the website
+sidebar = st.sidebar
+
+bitcoin_data = sidebar.checkbox("Bitcoin")
+if bitcoin_data:
+    display_crypto_data("Bitcoin")
+
+ethereum_data = sidebar.checkbox("Ethereum")
+if ethereum_data:
+    display_crypto_data("Ethereum")
+
+litecoin_data = sidebar.checkbox("Litecoin")
+if litecoin_data:
+    display_crypto_data("Litecoin")
+
+zcash_data = sidebar.checkbox("ZCash")
+if zcash_data:
+    display_crypto_data("ZCash")
+
+chainlink_data = sidebar.checkbox("Chainlink")
+if chainlink_data:
+    display_crypto_data("Chainlink")
+    
+    
---
+++ pages/2_CryptoNews.py
@@ -0,0 +1,69 @@
+import requests
+import datetime
+import streamlit as st
+from PIL import Image
+from newsapi import NewsApiClient
+
+# Loading icon image using PIL
+img = Image.open('./Images/1f4f0.png')
+
+# Adding title and icon image to app
+st.set_page_config(page_title='News', page_icon = img)
+
+# Remove main menu and footer defaults
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+st.title("Headlines")
+
+# Hardcoded api key for testing purposes
+newsapi = NewsApiClient(api_key='f52b9a0c753c4abb934b9b491356fe22')
+
+# List of coins
+coins =['Bitcoin', 'Ethereum', 'Litecoin', 'ZCash', 'Chainlink']
+
+# Create checkboxes in the sidebar
+selected_coins = st.sidebar.multiselect('Coins', coins)
+#selected_coins = st.sidebar.selectbox("Coins", options, format_func=lambda x: x.capitalize())
+
+
+# Only display headlines for selected coins
+if selected_coins:
+    for coin in selected_coins:
+        q = coin
+        everything = newsapi.get_everything(q=q,
+                                            language='en',
+                                            sort_by='relevancy',
+                                            page_size=5)
+        st.header(coin)
+        for article in everything['articles']:
+            st.write("[{}]({})".format(article['title'], article['url']), unsafe_allow_html=True)
+
+# Specify date range to display 
+today = datetime.datetime.now()
+week_ago = today - datetime.timedelta(days=7)
+
+# Bitcoin top-headlines
+everything = newsapi.get_everything(q='bitcoin', language='en', from_param=week_ago.strftime("%Y-%m-%d"), to=today.strftime("%Y-%m-%d"), page_size=5)
+
+# Ethereium top-headlines
+everything = newsapi.get_everything(q='ethereum', language='en', from_param=week_ago.strftime("%Y-%m-%d"), to=today.strftime("%Y-%m-%d"), page_size=5)
+
+# Litecoin top-headlines
+everything = newsapi.get_everything(q='litecoin', language='en', from_param=week_ago.strftime("%Y-%m-%d"), to=today.strftime("%Y-%m-%d"), page_size=5)
+
+# ZCash top-headlines
+everything = newsapi.get_everything(q='zcash', language='en', from_param=week_ago.strftime("%Y-%m-%d"), to=today.strftime("%Y-%m-%d"), page_size=5)
+
+# Chainlink top-headlines
+everything = newsapi.get_everything(q='Chainlink', language='en', from_param=week_ago.strftime("%Y-%m-%d"), to=today.strftime("%Y-%m-%d"), page_size=5)
+
+#print(everything)
+
+#This code uses the streamlit library to display news headlines related to Bitcoin, Ethereum, Litecoin, ZCash, and Chainlink. It imports the os, requests, datetime and streamlit libraries. It also imports the NewsApiClient from the newsapi library. The page title is set to 'News' and the page icon is set to a newspaper emoji. A hardcoded api key is used for testing purposes. A date range is specified for the articles that will be displayed. The code then uses a for loop to get the top-headlines related to each of the five cryptocurrencies mentioned above and displays them on the page with a link to each article.
+
